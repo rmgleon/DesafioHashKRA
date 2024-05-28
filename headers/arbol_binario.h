fab91cb299037c58c916cd49f48a241adbbc27a5
RMagnoLeon <magnoleonrossi@gmail.com>
2024-05-28 00:49:55 -0300

    Headers

---
+++ headers/arbol_binario.h
@@ -0,0 +1,15 @@
+#ifndef ARBOL_BINARIO_H
+#define ARBOL_BINARIO_H
+
+typedef struct tree{
+    int dato;
+    struct tree* left;
+    struct tree* right;
+}tree;
+
+tree *createTreeNode (int val);
+tree *addTreeNode (tree **arbol, int val);
+void showTree (tree *arbol);
+tree *findTreeNode (tree *arbol, int bus);
+
+#endif /* ARBOL_BINARIO_H */
