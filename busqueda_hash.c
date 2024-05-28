3a4abb138d1c033b7a47471bef1b9e1fa758f7f3
SgtTayon <taeeonkm@gmail.com>
2024-05-28 20:09:28 -0300

    Update busqueda_hash.c

--- busqueda_hash.c
+++ busqueda_hash.c
@@ -12,7 +12,11 @@ void FHash ( list *cadena[] ){
     printf("valor -> \t");
     scanf("%lls",&valor);
     fflush(stdin);
-
+    
+	clock_t start, end;
+ 	double cpu_time_used;
+	start = clock();
+						
     clave = (valor * (2*GB)) % TAM ;
     list * Find = cadena [clave];
 
@@ -28,4 +32,16 @@ void FHash ( list *cadena[] ){
     }
 
     printf("\n-- value not found --");
+
+    end = clock();				
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    if(cpu_time_used >=60){
+        	printf("Tomo %f minutos.\n", cpu_time_used/60);
+    }
+    else  if(cpu_time_used <=0){
+        	printf("Tomo %f ms.\n", cpu_time_used*60);
+    }
+    else{
+        	printf("Tomo %f segundoss.\n", cpu_time_used);
+    }
 }
