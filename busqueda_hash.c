17fd99713902cb223634a8ac5e0f9cb1c6448ae6
SgtTayon <taeeonkm@gmail.com>
2024-05-28 18:46:23 -0300

    Add files via upload

---
+++ busqueda_hash.c
@@ -0,0 +1,30 @@
+
+#include "headers/busqueda_hash.h"
+#include "headers/tabla_hash.h"
+
+
+// list * cadena [TAM];
+
+void FHash (){
+
+    long long int GB = 1073741824,clave, valor;
+    printf("valor -> \t");
+    scanf("%lls",&valor);
+    fflush(stdin);
+
+    clave = (valor * (2*GB)) % TAM ;
+    list * Find = cadena [clave];
+
+    while (Find){
+
+        if (Fnd ->datos == valor){
+
+            printf("\n-- value found --");
+        }
+
+    Find = Find -> siguiente ;
+
+    }
+
+    printf("\n-- value not found --");
+}
