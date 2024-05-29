2e96df521145875308f0c7979c4bbd512754147e
RMagnoLeon <magnoleonrossi@gmail.com>
2024-05-29 15:06:47 -0300

    Actualizado para ocupar la busqueda dicotomica

--- app.c
+++ app.c
@@ -4,20 +4,38 @@
 #include "headers/tabla_hash.h"
 #include "headers/arbol_binario.h"
 #include "headers/tabla_hash_mod.h"
+#include "headers/arreglo_binario.h"
 
+
+// Genera un arbol, lo llena de elementos, y permite buscar
+// elementos dentro de el definidos por el usuario
+// Muestra el tiempo que tardo en la busqueda
 void arbol();
+void arregloBusquedaBinaria(int arr[], int n);
+
+// Toma un hashmap (cadena) y una lista auxiliar.
+// Usa la lista para llenar a cadena de elementos
+// Permite la busqueda de elementos dentro del hashmap
+// deifindos por el usuario
+// Muestra el tiempo que tardo en la busqueda
 void hash(list *cadena[], list *lista);
 void hashMod(list *cadenaMod[], list *lista);
 
 int main(int argc, char *argv[]) {
-	// Seed the random number generator
     srand(time(NULL));
 	
+	// Arreglo para arregloBusquedaBinaria
+	int arreglo[1000000];
+	// Lista auxiliar de hash
 	list * lista = NULL;
+	// Hashmap de hash
 	list * cadena[TAM];
+	// Hashmap de hashMod
 	list * cadenaMod[TAM];
+	// Elementos de la lista auxiliar de hash
 	int N = 1000000;
 	
+	// Llena la lista auxiliar con elementos ordenados
 	for(int i=0; i<N; i++){
 		list * nuevo = (list *)malloc(sizeof(list));
 		nuevo->dato = i;
@@ -27,12 +45,13 @@ int main(int argc, char *argv[]) {
 
 	inicializarHash(lista, N, cadena);
 	inicializarHashMod(lista, N, cadenaMod);
+	createOrderedArray(arreglo,N);
 	
 	
 	int op;
 	
 	while(1){
-		printf("\n1- Hash \n2- Nan\n3- FHash\n4- Exit");
+		printf("\n1- Hash\n2- Arbol\n3- FHash\n4- Exit");
 		printf("\nOpcion: ");
 		scanf("%d", &op);
 		fflush(stdin);
@@ -51,6 +70,9 @@ int main(int argc, char *argv[]) {
 				printf("\n -- exit success --");
 				exit(0);
 			}break;
+			case 5:{
+				arregloBusquedaBinaria(arreglo,N);
+			}
 			default:{
 				printf("\n -- bad input, select 1-4 --");
 			}break;
@@ -70,10 +92,9 @@ void arbol(){
 	printf("-- Arbol --\n");
 
     int range = 1000000;
-	//int numbers[1000000];
     int *numbers = malloc((range + 1) * sizeof(int));
     if (numbers == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Error al inicializar el arreglo\n");
 		return;
     }
 
@@ -102,6 +123,7 @@ void arbol(){
 		scanf("%d", &valor);
 		start = clock();
 	
+
 		aux=findTreeNode(raiz,valor);
 		if(aux->dato == valor){
 			printf("Valor encontrado\n");
@@ -112,7 +134,7 @@ void arbol(){
 
 		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-		printf("Tomo %f segundos.\n", cpu_time_used);
+		printf("Tomo %f milisegundos.\n", cpu_time_used*1000);
 		
 
 	}while(valor!=-1);
@@ -126,12 +148,13 @@ void hash(list *cadena[], list *lista){
 	clock_t start, end;
     double cpu_time_used;
 
+	/*
 	start = clock();
     // Aca se cargaban las cosas
     end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Tomo %f segundos.\n", cpu_time_used);
-	
+	printf("Tomo %f milisegundos.\n", cpu_time_used*1000);
+	*/
     //printHash(cadena);
 	int valor=0;
 	do{
@@ -150,23 +173,25 @@ void hash(list *cadena[], list *lista){
 		end = clock();				
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-		printf("Tomo %f segundos.\n", cpu_time_used);
+		printf("Tomo %f milisegundos.\n", cpu_time_used*1000);
 
 	}while(valor!=-1);
 }
 
 void hashMod(list *cadenaMod[], list *lista){
+
 	printf("-- Hash Mod--");
 	
 	clock_t start, end;
     double cpu_time_used;
 
+	/*
 	start = clock();
     
     end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	printf("Tomo %f segundos.\n", cpu_time_used);
-
+	*/
 
     //printHash(cadena);
 	int valor;
@@ -184,10 +209,34 @@ void hashMod(list *cadenaMod[], list *lista){
 			printf("Valor no encontrado.\n");
 		}
 
-		end = clock();				
+		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("Tomo %f milisegundos.\n", cpu_time_used*1000);
 
-		printf("Tomo %f segundos.\n", cpu_time_used);
+	}while(valor!=-1);
+}
+
+void arregloBusquedaBinaria(int arr[], int n){
+	clock_t start, end;
+    double cpu_time_used;
+	int valor;
+	do{
+		start = clock();
+		
+		printf("Valor a buscar: ");
+		scanf("%d", &valor);
+		
+		if(binarySearch(arr,n,valor) != -1){
+			printf("Valor encontrado\n");
+		}
+
+		else{
+			printf("Valor no encontrado.\n");
+		}
+
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("Tomo %f milisegundos.\n", cpu_time_used*1000);
 
 	}while(valor!=-1);
 }
